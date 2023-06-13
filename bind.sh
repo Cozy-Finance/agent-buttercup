af7c546ce9a69c78d9421964168473e0fe68a5ec
Raghav Bansal <raghavb1234@gmail.com>
2023-06-13 10:59:20 -0400

    Add dummy token bindings

--- bind.sh
+++ bind.sh
@@ -14,6 +14,7 @@ forge install https://github.com/Cozy-Finance/cozy-protocol-v2 --no-commit
 forge install https://github.com/Cozy-Finance/cozy-models-v2-simulation --no-commit
 forge install https://github.com/Cozy-Finance/cozy-triggers-v2-simulation --no-commit
 forge install https://github.com/gnosis/canonical-weth --no-commit
+forge install https://github.com/Cozy-Finance/cozy-simulation-contracts --no-commit
 
 echo "Building contract artifacts ... "
 for d in ./contracts/*/ ; do (cd "$d" && [ -d "test" ] && mv "test" "test_ignore"); done
@@ -28,3 +29,4 @@ cargo run --bin binder -- --abis "contracts/cozy-triggers-v2-simulation/out" --b
 cargo run --bin binder -- --abis "contracts/cozy-models-v2-simulation/out" --bindings "crates/bindings" --mod-name "cozy_models" --overwrite
 cargo run --bin binder -- --abis "contracts/cozy-protocol-v2/out" --bindings "crates/bindings" --mod-name "cozy_protocol" --overwrite
 cargo run --bin binder -- --abis "contracts/canonical-weth/out" --bindings "crates/bindings" --mod-name "weth" --overwrite
+cargo run --bin binder -- --abis "contracts/cozy-simulation-contracts/out" --bindings "crates/bindings" --mod-name "cozy_simulation" --overwrite
---
+++ crates/bindings/src/cozy_simulation/metadata.rs
@@ -0,0 +1,7 @@
+pub use metadata::*;
+pub mod metadata {
+    pub static ERC20_NAME: &str = "ERC20";
+    pub static ERC20_PATH: &str = "lib/solmate/src/tokens/ERC20.sol";
+    pub static DUMMYTOKEN_NAME: &str = "DummyToken";
+    pub static DUMMYTOKEN_PATH: &str = "src/DummyToken.sol";
+}
