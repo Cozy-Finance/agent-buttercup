258a42ce27fbfc46ff0589bf74dff55a659d3e5c
chad-js <chadflemies@gmail.com>
2023-06-26 08:46:38 -0400

    rm unnecessary Address.into

--- crates/simulate/src/manager.rs
+++ crates/simulate/src/manager.rs
@@ -102,7 +102,7 @@ impl<U: UpdateData, W: World<WorldUpdateData = U>> SimManager<U, W> {
         }
 
         self.stepper
-            .insert_account_info(id.address.into(), new_agent.account_info());
+            .insert_account_info(id.address, new_agent.account_info());
 
         // Runs the agent's activation step and queue updates.
         let (sender, receiver) = unbounded::<AgentSimUpdate<U>>();
