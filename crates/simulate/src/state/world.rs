68a18dd6c0b11d2348563bb64ee2d24365f73986
Raghav Bansal <raghavb1234@gmail.com>
2023-07-11 17:36:55 -0400

    Remove stepper

--- crates/simulate/src/state/world.rs
+++ crates/simulate/src/state/world.rs
@@ -2,5 +2,5 @@ use crate::state::update::UpdateData;
 
 pub trait World: Sync + Send + Clone {
     type WorldUpdateData: UpdateData;
-    fn execute(&mut self, update: &Self::WorldUpdateData) -> Option<Self::WorldUpdateData>;
+    fn execute(&mut self, update: Self::WorldUpdateData) -> Option<Self::WorldUpdateData>;
 }
